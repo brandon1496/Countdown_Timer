66290e4e55c4fc674e008626923de06b744e47ef
Brandon Innis <brandoninnis@Brandons-MacBook-Pro.local>
2017-03-22 16:02:42 -0400

    Initial Commit

---
+++ Countdown_Timer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Countdown_Timer
+//
+//  Created by Brandon Innis on 3/22/17.
+//  Copyright © 2017 Brandon Innis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
